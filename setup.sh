b50071fc1d5ff188a3aa69670bcbf5b16ec17fbd
clement.l <maple3312936@outlook.com>
2024-07-19 14:54:34 +0800

    feat: initial setup

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Check if .venv has been initiated
+if [ ! -f .venv/bin/activate ]; then
+    virtualenv .venv
+fi
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+pip install -r requirements.txt -q
+poetry install
