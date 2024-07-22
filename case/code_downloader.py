8c56149b483ea5aed58dd99798d30ed8e885681a
clement.l <maple3312936@outlook.com>
2024-07-22 11:23:37 +0800

    feat: download meta

--- case/code_downloader.py
+++ case/code_downloader.py
@@ -6,13 +6,12 @@ from tqdm import tqdm
 
 
 class CaseDownloader:
-    def __init__(self, branch: str, output_dir: str = "raw_data/"):
+    def __init__(self, is_dev: bool = True, output_dir: str = "raw_data/"):
         load_dotenv()
-        if branch not in ["develop", "main"]:
-            raise ValueError("Branch not supported. Please use 'develop' or 'main'.")
+        self.is_dev: bool = is_dev
         self.access_token: str = os.getenv("GITHUB_ACCESS_TOKEN")
         self.repo: str = "blocto/bento-interface"
-        self.branch: str = branch
+        self.branch: str = "develop" if self.is_dev else "main"
         self.github_api_url: str = "https://api.github.com"
         self.output_dir: str = output_dir
 
@@ -43,28 +42,46 @@ class CaseDownloader:
     def _download_file(self, file_info: dict, path: str) -> None:
         """Download a single file from the repository."""
         content_encoded = file_info["content"]
-        print(content_encoded)
         file_content = base64.b64decode(content_encoded)
 
-        local_path = os.path.join(self.output_dir, path)
-        os.makedirs(os.path.dirname(local_path), exist_ok=True)
-
-        with open(local_path, "wb") as f:
+        with open(os.path.join(self.output_dir, path), "wb") as f:
             f.write(file_content)
+
+    def _download_meta(self):
+        """Download the metadata file from the repository."""
+        import json
+
+        env = "dev" if self.is_dev else "release"
+        url = f"https://bento-batch-{env}.netlify.app/case/api/meta"
+        response = requests.get(url)
+        response.raise_for_status()
+        cases = response.json()["cases"]
+        # Sort the cases by id alphabetically
+        cases_sorted = sorted(cases, key=lambda case: case["id"])
+        # Write the metadata to a file
+        with open(os.path.join(self.output_dir, "meta.json"), "w") as f:
+            json.dump(cases_sorted, f)
 
     def download(self) -> None:
         """Download all bento cases from the repository."""
         case_paths = self._get_case_path()
         base_url = f"{self.github_api_url}/repos/{self.repo}/contents"
 
-        for path in tqdm(case_paths, desc="Downloading cases", unit="file"):
+        # Download all cases
+        for path in tqdm(case_paths, desc="Downloading files", unit="file"):
+            local_dir = os.path.dirname(os.path.join(self.output_dir, path))
+            os.makedirs(local_dir, exist_ok=True)
+
             url = f"{base_url}/{path}"
             response = requests.get(url, headers=self.headers)
             response.raise_for_status()
             file_info = response.json()
             self._download_file(file_info, path)
 
+        # Download metadata
+        self._download_meta()
+
 
 if __name__ == "__main__":
-    downloader = CaseDownloader(branch="develop")
+    downloader = CaseDownloader(is_dev=True)
     downloader.download()
