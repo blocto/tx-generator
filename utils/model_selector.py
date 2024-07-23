4149e141d0d778960ec208f324bd2fd90782eee2
clement.l <maple3312936@outlook.com>
2024-07-23 12:31:27 +0800

    feat: add google vertex

--- utils/model_selector.py
+++ utils/model_selector.py
@@ -1,5 +1,6 @@
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
+from langchain_google_vertexai import ChatVertexAI
 
 
 def get_chat_model(name: str):
@@ -7,5 +8,7 @@ def get_chat_model(name: str):
         return ChatOpenAI(model=name, temperature=0)
     elif name in ["claude-3-5-sonnet-20240620"]:
         return ChatAnthropic(model=name, temperature=0)
+    elif name in ["gemini-1.5-flash-001", "gemini-1.5-pro-001"]:
+        return ChatVertexAI(model=name, temperature=0)
     else:
         raise ValueError(f"Unknown model name: {name}")
